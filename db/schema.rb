69337c544b3e691409ad69fc2bfb1d425a2f835f
vip-take <shuhei.taketani@gmail.com>
2016-09-19 16:54:17 +0900

    create prototype model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160916090003) do
+ActiveRecord::Schema.define(version: 20160919064256) do
+
+  create_table "prototypes", force: :cascade do |t|
+    t.text     "catchcopy",  limit: 65535
+    t.text     "concept",    limit: 65535
+    t.integer  "user_id",    limit: 4
+    t.string   "title",      limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255,   default: "", null: false
