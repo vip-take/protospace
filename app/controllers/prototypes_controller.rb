c36c8ca1664f0fb56f63caf18f3d273e600e5759
vip-take <shuhei.taketani@gmail.com>
2016-09-19 16:55:26 +0900

    add prototype post feature. (text only)

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,8 +3,22 @@ class PrototypesController < ApplicationController
   end
 
   def new
+    @proto = Prototype.new
+    @user = current_user.id
+  end
+
+  def create
+    Prototype.create(create_params)
+    redirect_to :root
   end
 
   def show
   end
+
+  private
+
+  def create_params
+    params.require(:prototype).permit(:title, :catchcopy, :concept).merge(user_id: current_user.id)
+  end
+
 end
