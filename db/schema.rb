9ec0dbb7588eb3f1e11c98a2f971df5f52b18d19
vip-take <shuhei.taketani@gmail.com>
2016-09-24 19:12:09 +0900

    change column type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160924081844) do
+ActiveRecord::Schema.define(version: 20160924100708) do
 
   create_table "images", force: :cascade do |t|
     t.integer  "prototype_id", limit: 4
@@ -35,7 +35,7 @@ ActiveRecord::Schema.define(version: 20160924081844) do
     t.string   "title",       limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "likes_count", limit: 4
+    t.integer  "likes_count", limit: 4,     default: 0
   end
 
   create_table "users", force: :cascade do |t|
