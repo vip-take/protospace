89806a37c67fa6d1cad1e1dae07794d2e2a6d515
vip-take <shuhei.taketani@gmail.com>
2016-09-25 23:54:33 +0900

    add realtime like reflection feature.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,7 +1,7 @@
 class Like < ActiveRecord::Base
 
   #counterをprototypeテーブルに挿入
-  belongs_to :prototype, :counter_cache => :likes_count
+  belongs_to :prototype, counter_cache: :likes_count
 
   belongs_to :user
 
