96df8860419c1e2c3fa2ecedab8d48759c81861b
vip-take <shuhei.taketani@gmail.com>
2016-09-26 15:23:58 +0900

    modify routing.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
 
   get  '/prototypes/index'  =>    'prototypes#index'
 
-  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy]
-  resources :likes, only: [:create, :update]
+  resources :prototypes, only: [:new, :create, :show, :edit, :update, :destroy] do
+    resources :likes, only: [:create, :destroy]
+  end
+
   resources :users, only: [:show, :edit, :update]
 end
