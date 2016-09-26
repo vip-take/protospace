3e567483da3f51761c608d8491ed1ef67f9d23fd
vip-take <shuhei.taketani@gmail.com>
2016-09-26 15:26:44 +0900

    modify prototype show page and controller.

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,19 +2,18 @@ class LikesController < ApplicationController
 
   def create
     @like = Like.create(create_params)
-    @proto = Prototype.find(params[:like][:prototype_id])
+    @proto = Prototype.find(params[:prototype_id])
   end
 
-  def update
+  def destroy
     Like.find(params[:id]).destroy
     @like = Like.new
-    @proto = Prototype.find(params[:like][:prototype_id])
-    @likes_count = @proto.likes_count
+    @proto = Prototype.find(params[:prototype_id])
   end
 
   private
   def create_params
-    params.require(:like).permit(:prototype_id).merge(user_id: current_user.id)
+    params.permit(:prototype_id).merge(user_id: current_user.id)
   end
 
 end
