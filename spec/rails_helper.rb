71e0522c44c519ec8a85ffdad12716e84edfdb5e
vip-take <shuhei.taketani@gmail.com>
2016-10-06 12:59:14 +0900

    modify rails_helper file.

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,8 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'spec_helper'
 require 'rspec/rails'
+require 'database_cleaner'
+require 'devise'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
@@ -36,6 +38,9 @@ RSpec.configure do |config|
 
   config.include ActionDispatch::TestProcess
 
+  #devise setting
+  config.include Devise::Test::ControllerHelpers, :type => :controller
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
@@ -64,4 +69,15 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
+
 end
