afad4438896bfb76ae30ee3a0b62adfb716fe452
vip-take <shuhei.taketani@gmail.com>
2016-10-07 14:19:42 +0900

    modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -54,6 +54,17 @@ namespace :deploy do
     end
   end
 
+  desc 'migrate database'
+  task :db_migrate do
+    on roles(:db) do |host|
+      with rails_env: fetch(:rails_env) do
+        within current_path do
+          execute :bundle, :exec, :rake, 'db:migrate', 'RAILS_ENV=production'
+        end
+      end
+    end
+  end
+
   desc 'Run seed'
   task :seed do
     on roles(:app) do
