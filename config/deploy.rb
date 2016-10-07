529587242010509cf5527312d469499037334703
vip-take <shuhei.taketani@gmail.com>
2016-10-07 10:56:04 +0900

    modify key in deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,8 +28,10 @@ set :rbenv_ruby, '2.3.1'
 #出力するログのレベル。
 set :log_level, :debug
 
-set :aws_access_key_id, ENV['ACCESS_KEY_ID']
-set :aws_secret_access_key, ENV['SECRET_ACCESS_KEY']
+set :default_env, {
+  AWS_ACCESS_KEY_ID: ENV['ACCESS_KEY_ID'],
+  AWS_SECRET_ACCESS_KEY: ENV['SECRET_ACCESS_KEY']
+}
 
 namespace :deploy do
   desc 'Restart application'
