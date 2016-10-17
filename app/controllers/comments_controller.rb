df51e27ae79c66ed2f0f6d9671a85ae31763b596
vip-take <shuhei.taketani@gmail.com>
2016-10-17 11:04:43 +0900

    add before_action authenticate_user! to comments and likes controller.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,14 @@
 class CommentsController < ApplicationController
+
+  before_action :authenticate_user!, only: [:create]
+
   def create
     Comment.create(create_params)
     @proto = Prototype.find(params[:prototype_id])
   end
 
   private
-    def create_params
+  def create_params
     params.require(:comment).permit(:text).merge(user_id: current_user.id,prototype_id: params[:prototype_id])
   end
 end
