1c46a2f95b5fde55386d13366b9efe5fd91b7278
vip-take <shuhei.taketani@gmail.com>
2016-10-17 18:19:48 +0900

    delete 'require pry-rails' in some files.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'pry-rails'
 
 describe UsersController do
   let(:user) { FactoryGirl.build(:user)}
--- spec/models/prototype_spec.rb
+++ spec/models/prototype_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'pry-rails'
 
 describe Prototype do
   let(:user) { FactoryGirl.build(:user)}
