b0f7d0a2bc4f5e73d19191e65d4151f749c42f65
vip-take <shuhei.taketani@gmail.com>
2016-10-17 11:06:23 +0900

    modify validation in image model.

--- app/models/image.rb
+++ app/models/image.rb
@@ -2,8 +2,10 @@ class Image < ActiveRecord::Base
 
   belongs_to :prototype
 
-  validates :photo, presence: true
-  validates :role, presence: true
+  # モデル単体でのテストのみon。以下を有効にすると、protoytypeモデル経由でアクセス不可
+  #http://ruby-rails.hatenadiary.com/entry/20141208/1418018874
+  # validates :photo, presence: true
+  # validates :role, presence: true
 
 # Carrierwave設定
   mount_uploader :photo, AvatarUploader
