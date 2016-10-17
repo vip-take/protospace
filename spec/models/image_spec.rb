a4b5768dc276382efc56a42c398ff522e8c4337d
vip-take <shuhei.taketani@gmail.com>
2016-10-17 17:31:32 +0900

    delete unnecessary tests.

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -10,18 +10,8 @@ describe Image do
       end
     end
     describe '異常系確認' do
-      it '画像が無い場合保存できないこと' do
-        image.photo = ""
-        expect(image.save).to be_falsey
-      end
-
       it '特定以外の画像の場合保存できないこと' do
         expect(main_ico.save).to be_falsey
-      end
-
-      it 'roleが無い場合保存できないこと' do
-        image.role = ""
-        expect(image.save).to be_falsey
       end
     end
   end
